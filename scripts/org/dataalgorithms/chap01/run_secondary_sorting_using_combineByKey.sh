7b14fb09dee1efe50e080a57d7ef1b2e3fbbf19e
michael.wh <michael.wh@alibaba-inc.com>
2018-03-01 14:09:09 +0800

    add chap01 shell script2

--- scripts/org/dataalgorithms/chap01/run_secondary_sorting_using_combineByKey.sh
+++ scripts/org/dataalgorithms/chap01/run_secondary_sorting_using_combineByKey.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_101.jdk/Contents/Home/
 echo "JAVA_HOME=$JAVA_HOME"
-#
+# 
 export BOOK_HOME=/Users/michael.wh/workspace/data-algorithms-book
 export SPARK_HOME=/Users/michael.wh/workspace/spark-2.2.0-bin-hadoop2.7
 export SPARK_MASTER=spark://localhost:7077
