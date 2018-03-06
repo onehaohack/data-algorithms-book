0b92e3fb515b9c6f1d7a3ac03f5ae2b199d1b13b
onehao <onehao333@gmail.com>
2018-03-06 20:12:29 +0800

    update run_secondary_sorting_using_combineByKey.sh

--- scripts/org/dataalgorithms/chap01/run_secondary_sorting_using_combineByKey.sh
+++ scripts/org/dataalgorithms/chap01/run_secondary_sorting_using_combineByKey.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_101.jdk/Contents/Home/
+export JAVA_HOME=/home/onehao/soft/jdk1.8.0_121
 echo "JAVA_HOME=$JAVA_HOME"
 # 
-export BOOK_HOME=/Users/michael.wh/workspace/data-algorithms-book
-export SPARK_HOME=/Users/michael.wh/workspace/spark-2.2.0-bin-hadoop2.7
-export SPARK_MASTER=spark://localhost:7077
+export BOOK_HOME=~/workspace/data-algorithms-book
+export SPARK_HOME=~/soft/spark-2.2.0-bin-hadoop2.7
+export SPARK_MASTER=spark://192.168.0.112:7077
 export SPARK_JAR=$BOOK_HOME/lib/spark-assembly-1.6.0-hadoop2.6.0.jar
 export APP_JAR=$BOOK_HOME/target/data-algorithms-1.0.0.jar
+export HDFS_HOME=hdfs://localhost:9000/user/onehao
 # define input parameters
-INPUT="file://$BOOK_HOME/time_series.txt"
-OUTPUT="file://$BOOK_HOME/output"
+#INPUT="file://$BOOK_HOME/time_series.txt"
+INPUT="hdfs://localhost:9000/user/onehao/workspace/data-algorithms-book/time_series.txt"
+OUTPUT="$HDFS_HOME/output"
+$HADOOP_HOME/bin/hadoop fs -rmr $OUTPUT
 #
 prog=org.dataalgorithms.chap01.spark.SecondarySortUsingCombineByKey
 $SPARK_HOME/bin/spark-submit  --class $prog --master $SPARK_MASTER $APP_JAR $INPUT $OUTPUT
