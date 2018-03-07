e4129484d8a086ac5676a454f55108529bd42748
michael.wh <michael.wh@alibaba-inc.com>
2018-03-07 19:12:57 +0800

    update topN hadoop shell.

--- scripts/org/dataalgorithms/chap03/mapreduce/run_top_N_mapreduce.sh
+++ scripts/org/dataalgorithms/chap03/mapreduce/run_top_N_mapreduce.sh
@@ -13,19 +13,27 @@
 #
 # @author Mahmoud Parsian
 #
+export BOOK_HOME=~/workspace/data-algorithms-book
+export SPARK_HOME=~/soft/spark-2.2.0-bin-hadoop2.7
+export SPARK_MASTER=spark://192.168.0.112:7077
+export SPARK_JAR=$BOOK_HOME/lib/spark-assembly-1.6.0-hadoop2.6.0.jar
+export APP_JAR=$BOOK_HOME/target/data-algorithms-1.0.0.jar
+export HDFS_HOME=hdfs://localhost:9000/user/onehao
 
-export JAVA_HOME=/usr/java/jdk7
-export HADOOP_HOME=/usr/local/hadoop-2.6.0
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-export BOOK_HOME=/home/data-algorithms-book
-export APP_JAR=$BOOK_HOME/dist/data_algorithms_book.jar
-export PATH=$HADOOP_HOME/bin:$JAVA_HOME/bin:$PATH
+
+#export JAVA_HOME=/usr/java/jdk7
+#export HADOOP_HOME=/usr/local/hadoop-2.6.0
+#export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+#export BOOK_HOME=/home/data-algorithms-book
+#export APP_JAR=$BOOK_HOME/dist/data_algorithms_book.jar
+#export PATH=$HADOOP_HOME/bin:$JAVA_HOME/bin:$PATH
+
 #
 # phase 1: aggregate keys
 #
 INPUT=/kv/input
 OUTPUT=/kv/output
-hadoop fs -rmr $OUTPUT
+hadoop fs -rm -r $OUTPUT
 AGGREGATOR=org.dataalgorithms.chap03.mapreduce.AggregateByKeyDriver
 hadoop jar $APP_JAR  $AGGREGATOR $INPUT $OUTPUT
 #
@@ -34,6 +42,6 @@ hadoop jar $APP_JAR  $AGGREGATOR $INPUT $OUTPUT
 N=5
 TopN_INPUT=/kv/output
 TopN_OUTPUT=/kv/final
-hadoop fs -rmr $TopN_OUTPUT
+hadoop fs -rm -r $TopN_OUTPUT
 TopN=org.dataalgorithms.chap03.mapreduce.TopNDriver
 hadoop jar $APP_JAR $TopN $N $TopN_INPUT $TopN_OUTPUT
--- scripts/org/dataalgorithms/chap03/spark/run_top10_spark_cluster.sh
+++ scripts/org/dataalgorithms/chap03/spark/run_top10_spark_cluster.sh
@@ -8,12 +8,15 @@
 #
 # @author Mahmoud Parsian
 #
+
+
 export BOOK_HOME=~/workspace/data-algorithms-book
 export SPARK_HOME=~/soft/spark-2.2.0-bin-hadoop2.7
 export SPARK_MASTER=spark://192.168.0.112:7077
 export SPARK_JAR=$BOOK_HOME/lib/spark-assembly-1.6.0-hadoop2.6.0.jar
 export APP_JAR=$BOOK_HOME/target/data-algorithms-1.0.0.jar
 export HDFS_HOME=hdfs://localhost:9000/user/onehao
+
 #
 INPUT=$BOOK_HOME/data/top10data.txt
 # Run on a Spark standalone cluster
